45a11e9cbdb7595c3bc5412f26a358c4ea3cae22
Mayur Pokharkar <mayurpokharkar6@gmail.com>
2022-01-10 11:10:34 +0530

    Done with TOPBAR

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo"> Socio-Club</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <Search className="searchIcon"></Search>
+          <input placeholder="Search Socio-Club" className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        {/*  */}
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">TimeLine</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        {/* Profile Picture */}
+        <img src="/assests/person/1.jpeg" alt="" className="topbarImage" />
+      </div>
+    </div>
+  );
+}
